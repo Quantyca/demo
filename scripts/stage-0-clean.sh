c54627b41530abafba0713f119ada5d6b06e3174
Andrea Gioia <andrea.gioia@quantyca.it>
2019-10-27 18:29:34 +0100

    Added dvc pipeline

---
+++ scripts/stage-0-clean.sh
@@ -0,0 +1,5 @@
+rm -f /opt/shared/data/processed/*.csv 2> /dev/null 
+rm -f /opt/shared/models/*.pkl 2> /dev/null 
+rm -f /opt/shared/metrics/*.metrics 2> /dev/null 
+rm -f /opt/shared/notebooks/outs/*.ipynb 2> /dev/null 
+rm -rf /opt/shared/notebooks/outs/.ipynb_checkpoints 2> /dev/null
